01eb97ba9e35f4472c3059f368ef24581aeeea8e
Aldiyar-B <aimamadiev@mail.ru>
2024-04-27 20:16:16 +0700

    сделал модальное окно, но не смог вывести по нажатию на кнопку

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var sliderTrack = document.querySelector('.slider__track');
-	var sliderItems = document.querySelectorAll('.slider__item');
-	var nextButton = document.querySelector('.slider__button--next');
-	var prevButton = document.querySelector('.slider__button--prev');
-	var position = 0; // Стартовая позиция слайдера
+	var modal = document.getElementById('modalSignUp');
+	var signUpButton = document.getElementById('sign-up');
+	var closeButton = document.querySelector('.close-modal'); // Adjust the class here to match your actual close button
 
-	// Функция для переключения слайдов
-	function moveTo(position) {
-		sliderTrack.style.transform = `translateX(${position}%)`;
+	function showModal() {
+		modal.classList.remove('hidden');
+		document.querySelector('.modal-background').style.display = 'block';
 	}
 
-	// Обработчики клика для кнопок
-	nextButton.addEventListener('click', function () {
-		if (position > -(100 * (sliderItems.length - 1))) {
-			position -= 100; // Двигаем слайдер на один слайд влево
-			moveTo(position);
-		} else {
-			position = 0; // Если достигнут последний слайд, возвращаемся к первому
-			moveTo(position);
-		}
-	});
+	function hideModal() {
+		modal.classList.add('hidden');
+		document.querySelector('.modal-background').style.display = 'none';
+	}
 
-	prevButton.addEventListener('click', function () {
-		if (position < 0) {
-			position += 100; // Двигаем слайдер на один слайд вправо
-			moveTo(position);
-		} else {
-			position = -(100 * (sliderItems.length - 1)); // Если достигнут первый слайд, переходим к последнему
-			moveTo(position);
-		}
-	});
-
-	// Инициализируем начальное положение слайдов
-	sliderItems.forEach((item, index) => {
-		item.style.left = `${index * 100}%`;
-	});
+	signUpButton.addEventListener('click', showModal);
+	closeButton.addEventListener('click', hideModal);
 });
